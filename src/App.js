0cd05afaf3cfc4f0023d66f6c9f03d27ccdbc3ce
GILB <barkan.gil@gmail.com>
2020-12-08 17:14:05 +0200

    pusher + axios in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import './App.css';
 import Chat from './components/Chat/Chat';
 import Sidebar from './components/Sidebar/Sidebar';
+import Pusher from 'pusher-js';
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync').then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('aa0f301a21b25380fc83', {
+      cluster: 'eu',
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function (data) {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
